f5a8ad0e8a71ab3a86ddfc632bf02f5a19f532d9
gkarlos <gkarloscodes@gmail.com>
2022-02-21 21:13:28 +0100

    accept root arg

--- zefi-2/script.py
+++ zefi-2/script.py
@@ -1,9 +1,10 @@
 import re
 import os
+import sys
 
 p = re.compile(".*[a-zA-Z]{4}[0-9]{5}$")
 
-for base, dirs, files in os.walk("/Users/gkarlos/Documents", topdown=True):
+for base, dirs, files in os.walk(os.path.abspath(sys.argv[1]), topdown=True):
     if p.match(base):
         print(base)
         del dirs
